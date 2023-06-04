5daa1248a6887d3ac474aae23e61d4ff82413075
Nyan <earthangle007@gmail.com>
2023-06-04 13:22:02 +0800

    edited pubspec

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,11 +8,8 @@ import Foundation
 import cloud_firestore
 import firebase_auth
 import firebase_core
-<<<<<<< HEAD
-import geolocator_apple
-=======
 import firebase_storage
->>>>>>> 4540e18a68ad4e7815a9df69ed418d66597f337e
+import geolocator_apple
 import location
 import path_provider_foundation
 
@@ -20,11 +17,8 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-<<<<<<< HEAD
-  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
-=======
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
->>>>>>> 4540e18a68ad4e7815a9df69ed418d66597f337e
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
