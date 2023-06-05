db28cb5dcf9253935df0e8e5fca5da52a7ac54a5
Nyan <earthangle007@gmail.com>
2023-06-06 00:24:14 +0800

    shorten code

--- lib/screens/browse.dart
+++ lib/screens/browse.dart
@@ -26,15 +26,73 @@ class _BrowseScreenState extends State<BrowseScreen> {
   //   super.dispose();
   // }
 
+  Widget _search(Map<String, dynamic> data, String posted) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GestureDetector(
+        onTap: () {
+          // setState(() {
+          //   searchController.text =
+          //       data[index]['module Code'];
+          // });
+        },
+        child: ListTile(
+          leading: SizedBox(
+            width: 50,
+            height: 50,
+            child: Image(
+              image: NetworkImage(
+                data['image_url'],
+              ),
+              fit: BoxFit.fill,
+            ),
+          ),
+          title: Text(data['query']),
+          subtitle: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(data['module Code']),
+                  Text(data['cost']),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(data['mentee']),
+                  Text('$posted min'),
+                ],
+              ),
+            ],
+          ),
+          trailing: const Icon(Icons.arrow_right),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: TextField(
-          decoration: const InputDecoration(
+          controller: searchController,
+          decoration: InputDecoration(
               hintText: 'Search Module Code',
-              prefixIcon: Icon(Icons.search_rounded),
-              suffixIcon: Icon(Icons.close_rounded)),
+              prefixIcon: const Icon(Icons.search_rounded),
+              suffixIcon: IconButton(
+                onPressed: () {
+                  setState(() {
+                    searchController.clear();
+                    code = "";
+                  });
+                },
+                icon: const Icon(Icons.close_rounded),
+              )),
           onChanged: (val) {
             setState(() {
               code = val;
@@ -83,107 +141,13 @@ class _BrowseScreenState extends State<BrowseScreen> {
                     var posted = diff.inMinutes.toString();
 
                     if (code.trim().isEmpty) {
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: GestureDetector(
-                          onTap: () {
-                            // setState(() {
-                            //   searchController.text =
-                            //       data[index]['module Code'];
-                            // });
-                          },
-                          child: ListTile(
-                            leading: SizedBox(
-                              width: 50,
-                              height: 50,
-                              child: Image(
-                                image: NetworkImage(
-                                  data['image_url'],
-                                ),
-                                fit: BoxFit.fill,
-                              ),
-                            ),
-                            title: Text(data['query']),
-                            subtitle: Column(
-                              children: [
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(data['module Code']),
-                                    Text(data['cost']),
-                                  ],
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(data['mentee']),
-                                    Text('$posted min'),
-                                  ],
-                                ),
-                              ],
-                            ),
-                            trailing: const Icon(Icons.arrow_right),
-                          ),
-                        ),
-                      );
+                      return _search(data, posted);
                     }
                     if (data['module Code']
                         .toString()
                         .toLowerCase()
                         .contains(code.toLowerCase())) {
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: GestureDetector(
-                          onTap: () {
-                            // setState(() {
-                            //   searchController.text =
-                            //       _foundqueries[index]['module Code'];
-                            // });
-                          },
-                          child: ListTile(
-                            leading: SizedBox(
-                              width: 50,
-                              height: 50,
-                              child: Image(
-                                image: NetworkImage(
-                                  data['image_url'],
-                                ),
-                                fit: BoxFit.fill,
-                              ),
-                            ),
-                            title: Text(data['query']),
-                            subtitle: Column(
-                              children: [
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(data['module Code']),
-                                    Text(data['cost']),
-                                  ],
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(data['mentee']),
-                                    Text('$posted min'),
-                                  ],
-                                ),
-                              ],
-                            ),
-                            trailing: const Icon(Icons.arrow_right),
-                          ),
-                        ),
-                      );
+                      return _search(data, posted);
                     }
                     return Container();
                   },
